f46f1d2d90dc874f1619a2386bfd30d8403a8463
LudzCPP <kacper.janowicz@onet.pl>
2021-09-02 13:41:41 +0200

    Add audio download

--- YoutubeDownloader.py
+++ YoutubeDownloader.py
@@ -1,30 +1,60 @@
 import pytube
 import tkinter as tk
-from PIL import ImageTk, Image
 
 
 class YoutubeDownloader(tk.Tk):
 	def __init__(self):
 		super().__init__()
 		self.title("Youtube Downloader by Ludz")
-		self.geometry("500x300")
-
-		# # logo
-		# self.logo_image = ImageTk.PhotoImage(Image.open("yt_logo.png").resize((500, 150)))
-		# self.logo = tk.Label(image=self.logo_image)
-		# self.logo.grid(row=0, column=0)
+		self.geometry("500x450")
 
 		self.window_top = tk.Label(text="Download a video!", font=("Comic Sans MS", 30, "bold"))
 		self.window_top.pack(pady=30)
 
 		self.url_input = tk.Entry(width=50)
-		self.url_input.pack()
+		self.url_input.pack(pady=20)
 
-		self.submit_button = tk.Button(text="Download", width=15, height=1, font=("Arial", 20), command=self.download_video)
-		self.submit_button.pack(pady=20)
+		self.video_title = tk.Label(text="\n", font=("Arial", 13))
+		self.video_title.pack()
+
+		self.video_button = tk.Button(
+			text="Download video",
+			width=15,
+			height=1,
+			font=("Arial", 20),
+			command=lambda:self.download_video(self.url_input.get())
+		)
+		self.video_button.pack(pady=20)
+
+		self.audio_button = tk.Button(
+			text="Download audio",
+			width=15,
+			height=1,
+			font=("Arial", 20),
+			command=lambda:self.download_audio(self.url_input.get())
+		)
+		self.audio_button.pack(pady=20)
 
 		self.mainloop()
 
-	def download_video(self):
-		video = pytube.YouTube(self.url_input.get())
-		video.streams.get_highest_resolution().download()
+	def download_video(self, url):
+		try:
+			pytube.YouTube(url).check_availability()
+		except:
+			self.video_title.config(text="Video not found!\n", font=("Arial", 13))
+		else:
+			video_url = pytube.YouTube(url)
+			video = video_url.streams.get_highest_resolution()
+			video.download()
+			self.video_title.config(text=f"Downloading...\n {video.title}", font=("Arial", 13))
+
+	def download_audio(self, url):
+		try:
+			pytube.YouTube(url).check_availability()
+		except:
+			self.video_title.config(text="Video not found!\n", font=("Arial", 13))
+		else:
+			video_url = pytube.YouTube(url)
+			audio = video_url.streams.get_audio_only()
+			audio.download()
+			self.video_title.config(text=f"Downloading...\n{audio.title}", font=("Arial", 13))
